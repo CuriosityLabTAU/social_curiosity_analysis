20be0a462d3ed7c6f5c6d65b62e26e80a4d4d3d0
“cltl1t405” <gorenlaptop1t405@gmail.com>
2018-09-16 09:51:42 +0300

    rosbag analasis wip 2

--- analayze_rosbag.py
+++ analayze_rosbag.py
@@ -17,15 +17,16 @@ files = [f for f in listdir(mypath) if isfile(join(mypath, f)) and '.bag' in f]
 count =0
 data = {}
 
+position_inv = {'left': 0, 'center': 1, 'right': 2}
+
 # for each bag file
 for f in files:
     info = f.split('_')
     subject_id = float(info[4])
 
     if subject_id > 0.0:
-        print('processing ', subject_id)
+        print('processing ', subject_id,"count= ",count)
         count+=1
-        print "count= ",count
         data[subject_id] = {}
         data[subject_id][0] = {}
 
@@ -47,6 +48,9 @@ for f in files:
         looking_at_dict={0:0, 1:0, 2:0}
         collect_secondary_robots={0:False, 1:False, 2:False}
 
+        tracker_dict={0:0, 1:0, 2:0}
+        collect_tracker={0:False, 1:False, 2:False}
+
 
 
         try:
@@ -60,22 +64,26 @@ for f in files:
 
                             data[subject_id][section_id] = {}
 
-                #get turn
+
+
+
+                #get turn info
                 if 'log' in topic:
                     if 'start:' in msg.data:
                         if 'turn:' in msg.data:
-                            turn = int(msg.data[5])
-                            data[subject_id][section_id][turn] = {'secondary_robots_data':{}}
+                            turn_data= msg.data.split(':')
+                            turn = int(turn_data[1])
+                            main=turn_data[-1]
+                            if main=='h':
+                                number_of_secondary=3
+                            else:
+                                main=int(main)
+                                number_of_secondary=2
 
+                            data[subject_id][section_id][turn] = {'secondary_robots_data':{}, 'main':main, 'number_of_secondary':number_of_secondary}
 
-                # #get tracker aggregation:
-                # if 'log' in topic:
-                #     if 'robot_counts:' in msg.data:
-                #         tracker_aggregation= json.loads(msg.data.split(':')[1][:])
-                #         data[subject_id][section_id][turn]['tracker_aggregation'] =tracker_aggregation
-                #
-                #         if sum(tracker_aggregation) < 10:
-                #             print ('Problem with eye tracker-subject:'+str(subject_id)+'-section:'+str(section_id)+'turn:'+str(turn)+'-sum is:'+str(sum(tracker_aggregation)))
+                            # collect eye tracker data on the turn
+                            collect_tracker = {0: True, 1: True, 2: True}
 
 
                 #get question data:
@@ -94,7 +102,7 @@ for f in files:
                         data[subject_id][section_id][current_question] = {'right_answer':right_answer,'subject_answer':subject_answer,'was_correct':right_answer==subject_answer}
 
 
-                #get looking at behaviour data:
+                ##get looking at behaviour data:
                 if 'log' in topic:
                     if 'secondary' in msg.data:
                         secondary_data= msg.data.split(":")
@@ -102,31 +110,38 @@ for f in files:
                         data[subject_id][section_id][turn]['secondary_robots_data'][secondary_robot]={'behavior': ":".join(secondary_data[2:-2]),'relationship':secondary_data[-1]}
                         collect_secondary_robots[secondary_robot]=True
 
+                #tracker info:
                 if 'log' in topic:
                     if 'all:back_to_sit' in msg.data:
-                        #stop colecting
-                        #update dict with the number of lookes
+                        for i in xrange(3):
+                            if collect_secondary_robots[i]:
+                                data[subject_id][section_id][turn]['secondary_robots_data'][i]['look_data']=looking_at_dict[i]
 
-                #colect lookes
+                        data[subject_id][section_id][turn]['tracker_aggregation'] = tracker_dict
+
+                        sum_tracking=tracker_dict[0]+tracker_dict[1]+tracker_dict[2]
+                        if sum_tracking < 10:
+                            print ('Problem with eye tracker-subject:'+str(subject_id)+'-section:'+str(section_id)+'turn:'+str(turn)+'-sum is:'+str(sum_tracking))
 
 
-                #colect the total lookes
+                        #restart counters
+                        collect_secondary_robots = {0: False, 1: False, 2: False}
+                        looking_at_dict = {0: 0, 1: 0, 2: 0}
+
+                        tracker_dict = {0: 0, 1: 0, 2: 0}
+                        collect_tracker = {0: False, 1: False, 2: False}
+
+
+                if 'eye_tracking' in topic:
+                    direction=msg.data
+                    position=position_inv[direction]
+                    if collect_secondary_robots[position]:
+                        looking_at_dict[position]+=1
+
+                    if collect_tracker[position]:
+                        tracker_dict[position]+=1
 
                 #give number of robot that are secondery
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         except:
             print('error - subject_id: ',subject_id)
@@ -135,162 +150,6 @@ for f in files:
 
 print data
 
-# sections = ['learn', 'task1', 'task2', 'task3']
-# #
-# subjects_with_no_angles_topic=[]
-
-
-#
-#
-#         #is_nao_angles_topic
-#         is_nao_angles_topic=False
-#
-#         is_working= False
-#
-#         current_skeleton_angle = None
-#         current_nao_movements = None
-#         current_nao_command = None
-#
-#         first_time = True
-#
-#         # try:
-#
-#         for topic, msg, t in bag.read_messages():
-#             # get the first time, for reference
-#             if first_time:
-#                 t0 = t
-#                 first_time = False
-#
-#             if 'log' in topic:
-#                 if'current_state' in msg.data:
-#                     step=int(msg.data.split(': ')[1])
-#                     if step in data[subject_id]:
-#                         continue
-#
-#                     data[subject_id][step]={}
-#                     if step == 1:
-#                         section_id = 1
-#                         print 'here'
-#                     else:
-#                         section_id = 0
-#
-#                 if 'matrix' in msg.data:
-#                     if step in data[subject_id]:
-#                         matrix=msg.data.split(':')[1]
-#                         data[subject_id][step]['matrix'] = np.array(np.matrix([[float(x)for x in row if x!=''] for row in [line[2:-1].split(' ') for line in  matrix[2:-1].split('\n ')]]))
-#
-#
-#                 if'task' in msg.data:
-#                     task_full_name=msg.data.split(',')[2]
-#                     task=task_full_name.split('/')[-1]
-#
-#
-#             if 'flow' in topic:
-#                 #   get the transformation
-#
-#                 if msg.data.isdigit():
-#                     if step in data[subject_id]:
-#                         data[subject_id][step]['transformation'] = int(msg.data)
-#
-#                   # parse the time to sections (learning, task1, task2, task3)
-#                 if 'start' in msg.data:
-#                     if step in data[subject_id]:
-#                         is_working=True
-#                         data[subject_id][step][sections[section_id]] = {
-#                             'start': t,
-#                             'stop': None,
-#                             'data': [],
-#                             'task':task
-#                         }
-#
-#                 if 'stop' in msg.data:
-#                     if step in data[subject_id]:
-#                         if sections[section_id] in data[subject_id][step]:
-#                             is_working=False
-#                             data[subject_id][step][sections[section_id]]['stop'] = t
-#                             task = 0
-#                             section_id += 1
-#
-#                             if step == 12:
-#                                 bag.close()
-#                                 break
-#
-#                 if section_id >= len(sections):
-#                     section_id=0
-#
-#
-#             if 'affdex' in topic:
-#                 # print msg
-#                 dict = {}
-#                 for m in affdex_list:
-#                     dict[m] = eval('msg.' + m)
-#                     if m == 'face_points':
-#                         n_list = []
-#                         o_list = dict[m]
-#                         for point in o_list:
-#                             n_list.append({"x": point.x, "y": point.y})
-#                         dict[m] = n_list
-#                 affdex = dict
-#
-#
-#               # for each section
-#             # if 'nao_movements' in topic:
-#             #    #          get command to robot
-#             #     current_nao_movements = msg.data
-#
-#
-#             # time between skeleton_angles is +- 0.04
-#             # time between robot angles is +- 0.09
-#
-#
-#             if 'nao_angles_topic' in topic:
-#                 #       get command to robot
-#
-#                 is_nao_angles_topic = True
-#
-#                 list_of_movements_str = msg.data
-#                 list_of_movements = list_of_movements_str[1:-1].split(', ')
-#
-#                 current_nao_movements = (list_of_movements[2:4]+[0.0,0.0]+list_of_movements[-6:-4]+[0.0,0.0])
-#
-#                 current_nao_movements= str([float(x) for x in current_nao_movements])
-#                 current_nao_movements= current_nao_movements[1:-1]
-#
-#             if 'to_nao' in topic:
-#                 #       get command to robot
-#                 if'change_pose' in msg.data:
-#                     current_nao_command = msg.data
-#
-#             if 'skeleton_angle' in topic:
-#                 #       get raw skeleton markers
-#                 current_skeleton_angle = msg.data
-#                 # print current_skeleton_angle
-#
-#                     #dict[id] = dict[section] = array(dict{skeleton, robot, time})
-#                 if is_working==True:
-#                     if step in data[subject_id]:
-#                         if sections[section_id] in data[subject_id][step]:
-#                             if current_skeleton_angle is not None and current_nao_movements is not None and current_nao_command is not None:
-#                                 new_data = {
-#                                     'time': (t - t0).to_sec(),
-#                                     'skeleton': current_skeleton_angle,
-#                                     'robot_cimmand':current_nao_command,
-#                                     'robot': current_nao_movements,
-#                                     'affdex':affdex
-#                                 }
-#                                 data[subject_id][step][sections[section_id]]['data'].append(new_data)
-#
-#         if is_nao_angles_topic==False:
-#             subjects_with_no_angles_topic.append(subject_id)
-#
-#         # except:
-#         #     print('error')
-#         #     data.pop(subject_id)
-#
-#
 # print(data.keys())
-# print 'subjects_with_no_angles_topic: ',subjects_with_no_angles_topic
-#
-# pickle.dump(obj=data, file=open('data/raw_data_28_6', 'wb'))
-#
-#
+
+# pickle.dump(obj=data, file=open('data/raw_data_28_6', 'wb'))#
