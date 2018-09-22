a8db2e75a81f71e9fdaf548e30241c0f6b05ebe5
“cltl1t405” <gorenlaptop1t405@gmail.com>
2018-09-22 12:52:52 +0300

    data from pt1 to matan and noga  - to csv

--- preprosses_pt.1.py
+++ preprosses_pt.1.py
@@ -79,20 +79,129 @@ AQ_data.set_index('pt1uniqueID',inplace=True)
 AQ_score=AQ_scoring(AQ_data)
 
 
+## NARS data:
+NARS_data=pt_1_data[['pt1uniqueID','NARS_1', 'NARS_2', 'NARS_3', 'NARS_4', 'NARS_5', 'NARS_6', 'NARS_7', 'NARS_8', 'NARS_9', 'NARS_10', 'NARS_11',
+       'NARS_12', 'NARS_13', 'NARS_14']]
+
+NARS_data.columns = ['pt1uniqueID']+[i for i in xrange(1,15)]
+
+for col in list(NARS_data):
+       NARS_data[col] = NARS_data[col].astype(float)
+
+NARS_data.set_index('pt1uniqueID',inplace=True)
+
+#scoring:
+# NARS_score=NARS_score._scoring(NARS_data)
+
 #personal_info:
-personal_info_data=pt_1_data[['pt1uniqueID','concent','age', 'Q20', 'city', 'email', 'psychometric', 'grades avg.','Q24']]
-
-#validation
-personal_info_data=personal_info_data[personal_info_data.concent=='1']
-
-personal_info_data.columns = ['pt1uniqueID']+['concent','age', 'gender', 'city', 'email', 'pet', 'avg_grades','faculty']
+# personal_info_data=pt_1_data[['pt1uniqueID','concent','age', 'Q20', 'city', 'email', 'psychometric', 'grades avg.','Q24']]
+#
+# #validation
+# personal_info_data=personal_info_data[personal_info_data.concent=='1']
+#
+# personal_info_data.columns = ['pt1uniqueID']+['concent','age', 'gender', 'city', 'email', 'pet', 'avg_grades','faculty']
 
 # for col in list ['pt1uniqueID','concent','age', 'gender','pet', 'avg_grades']:
 #        personal_info_data[col] = personal_info_data[col].astype(float)
 
 
 
+# print personal_info_data
 
-print personal_info_data
+
+# save all pt_1 scored data:
 
 
+def save_data_to_csv_for_matan_and_noga(pt_1_data):
+       # save data as csv for matan and noga:
+       # BEAQ data
+       BEAQ_data=pt_1_data[['pt1uniqueID', 'BEAQ_1', 'BEAQ_2', 'BEAQ_3', 'BEAQ_4', 'BEAQ_5', 'BEAQ_6',
+              'BEAQ_7', 'BEAQ_8', 'BEAQ_9', 'BEAQ_10', 'BEAQ_11', 'BEAQ_12', 'BEAQ_13', 'BEAQ_14', 'BEAQ_15']]
+
+       BEAQ_data.columns = ['pt1uniqueID']+[i for i in xrange(1,16)]
+
+       for col in list(NARS_data):
+              NARS_data[col] = BEAQ_data[col].astype(float)
+
+       BEAQ_data.set_index('pt1uniqueID',inplace=True)
+
+       BEAQ_data.to_csv('data/data_for_matan_and_noga/BEAQ_data.csv')
+
+       # LSAS_1 data
+       LSAS_1_data=pt_1_data[['pt1uniqueID', 'LSAS#1_1', 'LSAS#1_2', 'LSAS#1_3',
+              'LSAS#1_4', 'LSAS#1_5', 'LSAS#1_6', 'LSAS#1_7', 'LSAS#1_8', 'LSAS#1_9', 'LSAS#1_10', 'LSAS#1_11', 'LSAS#1_12', 'LSAS#1_13', 'LSAS#1_14',
+              'LSAS#1_15', 'LSAS#1_16', 'LSAS#1_17', 'LSAS#1_18', 'LSAS#1_19', 'LSAS#1_20', 'LSAS#1_21', 'LSAS#1_22','LSAS#1_23', 'LSAS#1_24']]
+
+       LSAS_1_data.columns = ['pt1uniqueID']+[i for i in xrange(1,25)]
+
+       for col in list(LSAS_1_data):
+              LSAS_1_data[col] = LSAS_1_data[col].astype(float)
+
+       LSAS_1_data.set_index('pt1uniqueID',inplace=True)
+
+       LSAS_1_data.to_csv('data/data_for_matan_and_noga/LSAS_1_data.csv')
+
+
+       # LSAS_2 data
+       LSAS_2_data=pt_1_data[['pt1uniqueID', 'LSAS#2_1',
+              'LSAS#2_2', 'LSAS#2_3', 'LSAS#2_4', 'LSAS#2_5', 'LSAS#2_6', 'LSAS#2_7', 'LSAS#2_8', 'LSAS#2_9', 'LSAS#2_10', 'LSAS#2_11', 'LSAS#2_12', 'LSAS#2_13',
+              'LSAS#2_14', 'LSAS#2_15', 'LSAS#2_16', 'LSAS#2_17', 'LSAS#2_18', 'LSAS#2_19', 'LSAS#2_20', 'LSAS#2_21', 'LSAS#2_22', 'LSAS#2_23', 'LSAS#2_24']]
+
+       LSAS_2_data.columns = ['pt1uniqueID']+[i for i in xrange(1,25)]
+
+       for col in list(LSAS_2_data):
+              LSAS_2_data[col] = LSAS_2_data[col].astype(float)
+
+       LSAS_2_data.set_index('pt1uniqueID',inplace=True)
+
+       LSAS_2_data.to_csv('data/data_for_matan_and_noga/LSAS_2_data.csv')
+
+
+       # AAQ-2 data
+       AAQ_2_data=pt_1_data[['pt1uniqueID', 'AAQ-2_1','AAQ-2_2', 'AAQ-2_3', 'AAQ-2_4', 'AAQ-2_5', 'AAQ-2_6', 'AAQ-2_7', 'AAQ-2_8', 'AAQ-2_9', 'AAQ-2_10']]
+
+       AAQ_2_data.columns = ['pt1uniqueID']+[i for i in xrange(1,11)]
+
+       for col in list(AAQ_2_data):
+              AAQ_2_data[col] = AAQ_2_data[col].astype(float)
+
+       AAQ_2_data.set_index('pt1uniqueID',inplace=True)
+
+       AAQ_2_data.to_csv('data/data_for_matan_and_noga/AAQ_2_data.csv')
+
+
+       # GAD7 data
+       GAD7_data=pt_1_data[['pt1uniqueID', 'GAD7_1', 'GAD7_2', 'GAD7_3', 'GAD7_4', 'GAD7_5', 'GAD7_6',
+              'GAD7_7', 'GAD7_8']]
+
+       GAD7_data.columns = ['pt1uniqueID']+[i for i in xrange(1,9)]
+
+       for col in list(GAD7_data):
+              GAD7_data[col] = GAD7_data[col].astype(float)
+
+       GAD7_data.set_index('pt1uniqueID',inplace=True)
+
+       GAD7_data.to_csv('data/data_for_matan_and_noga/GAD7_data.csv')
+
+
+       # TCI-140 data
+       TCI_140_data=pt_1_data[['pt1uniqueID', 'TCI-140_1', 'TCI-140_2', 'TCI-140_3', 'TCI-140_4', 'TCI-140_5', 'TCI-140_6', 'TCI-140_7', 'TCI-140_8', 'TCI-140_9', 'TCI-140_10',
+              'TCI-140_11', 'TCI-140_12', 'TCI-140_13', 'TCI-140_14', 'TCI-140_15', 'TCI-140_16', 'TCI-140_17', 'TCI-140_18', 'TCI-140_19', 'TCI-140_20', 'TCI-140_21',
+              'TCI-140_22', 'TCI-140_23', 'TCI-140_24', 'TCI-140_25', 'TCI-140_26', 'TCI-140_27', 'TCI-140_28', 'TCI-140_29', 'TCI-140_30', 'TCI-140_31', 'TCI-140_32',
+              'TCI-140_33', 'TCI-140_34', 'TCI-140_35', 'TCI-140_36', 'TCI-140_37', 'TCI-140_38', 'TCI-140_39', 'TCI-140_40', 'TCI-140_41', 'TCI-140_42', 'TCI-140_43',
+              'TCI-140_44', 'TCI-140_45', 'TCI-140_46', 'TCI-140_47', 'TCI-140_48', 'TCI-140_49', 'TCI-140_50', 'TCI-140_51', 'TCI-140_52', 'TCI-140_53', 'TCI-140_54',
+              'TCI-140_55', 'TCI-140_56', 'TCI-140_57', 'TCI-140_58', 'TCI-140_59', 'TCI-140_60', 'TCI-140_61', 'TCI-140_62', 'TCI-140_63', 'TCI-140_64', 'TCI-140_65',
+              'TCI-140_66', 'TCI-140_67', 'TCI-140_68', 'TCI-140_69', 'TCI-140_70', 'TCI-140_71', 'TCI-140_72', 'TCI-140_73', 'TCI-140_74', 'TCI-140_75', 'TCI-140_76',
+              'TCI-140_77', 'TCI-140_78', 'TCI-140_79', 'TCI-140_80']]
+
+       TCI_140_data.columns = ['pt1uniqueID']+[i for i in xrange(1,81)]
+
+       for col in list(TCI_140_data):
+              TCI_140_data[col] = TCI_140_data[col].astype(float)
+
+       TCI_140_data.set_index('pt1uniqueID',inplace=True)
+
+
+       TCI_140_data.to_csv('data/data_for_matan_and_noga/TCI_140_data.csv')
+
+# save_data_to_csv_for_matan_and_noga(pt_1_data)
