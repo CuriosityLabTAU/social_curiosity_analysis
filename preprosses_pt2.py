da3478ad7ba27db4ee4b5c8a03dfc8055e9f4f8c
Matan <matanep@gmail.com>
2018-09-24 16:00:15 +0300

    state trait

--- preprosses_pt2.py
+++ preprosses_pt2.py
@@ -6,7 +6,7 @@ sys.path.append("utils_for_questioners")
 from CEI_2_scoring import CEI_2_scoring
 from Five_Dimensional_Curiosity_scoring import Five_Dimensional_Curiosity_scoring
 from SCS_scoring import SCS_scoring
-
+from State_trait_scale_scoring import State_trait_scale_scoring
 
 pt_2_data=pd.read_csv('data/qualtrics/pt.2/Pt.2_September+22%2C+2018_14.23.csv')
 all_c=['StartDate', 'EndDate', 'Status', 'IPAddress', 'Progress', 'Duration (in seconds)', 'Finished', 'RecordedDate', 'ResponseId',
@@ -64,7 +64,7 @@ Godspeed_data.set_index('unique_ID',inplace=True)
 # Godspeed_score=Godspeed_scoring(Godspeed_data)
 
 ##State_trait_scale_1 data
-State_trait_scale_1_data=pt_2_data[['unique ID','State-trait scale 1_1', 'State-trait scale 1_2', 'State-trait scale 1_3', 'State-trait scale 1_4',
+State_trait_scale_data=pt_2_data[['unique ID','State-trait scale 1_1', 'State-trait scale 1_2', 'State-trait scale 1_3', 'State-trait scale 1_4',
                                     'State-trait scale 1_5',
        'State-trait scale 1_6', 'State-trait scale 1_7', 'State-trait scale 1_8', 'State-trait scale 1_9', 'State-trait scale 1_10',
        'State-trait scale 1_11', 'State-trait scale 1_12', 'State-trait scale 1_13', 'State-trait scale 1_14', 'State-trait scale 1_15',
@@ -72,42 +72,26 @@ State_trait_scale_1_data=pt_2_data[['unique ID','State-trait scale 1_1', 'State-
        'State-trait scale 1_21', 'State-trait scale 1_22', 'State-trait scale 1_23', 'State-trait scale 1_24', 'State-trait scale 1_25',
        'State-trait scale 1_26', 'State-trait scale 1_27', 'State-trait scale 1_28', 'State-trait scale 1_29', 'State-trait scale 1_30',
        'State-trait scale 1_31', 'State-trait scale 1_32', 'State-trait scale 1_33', 'State-trait scale 1_34', 'State-trait scale 1_35',
-       'State-trait scale 1_36', 'State-trait scale 1_37', 'State-trait scale 1_38', 'State-trait scale 1_39', 'State-trait scale 1_40']]
-
-State_trait_scale_1_data.columns = ['unique_ID']+[i for i in xrange(1,41)]
-
-for col in list(State_trait_scale_1_data):
-       State_trait_scale_1_data[col] = State_trait_scale_1_data[col].astype(float)
-
-State_trait_scale_1_data.set_index('unique_ID',inplace=True)
-
-# scoring:
-# State_trait_scale_1_score=State_trait_scale_1_scoring(State_trait_scale_1_data)
-
-
-##State_trait_scale_2 data
-State_trait_scale_2_data=pt_2_data[['unique ID','State-trait scale 2_1', 'State-trait scale 2_2', 'State-trait scale 2_3', 'State-trait scale 2_4',
-                                    'State-trait scale 2_5',
+       'State-trait scale 1_36', 'State-trait scale 1_37', 'State-trait scale 1_38', 'State-trait scale 1_39', 'State-trait scale 1_40',
+        'State-trait scale 2_1', 'State-trait scale 2_2', 'State-trait scale 2_3', 'State-trait scale 2_4','State-trait scale 2_5',
        'State-trait scale 2_6', 'State-trait scale 2_7', 'State-trait scale 2_8', 'State-trait scale 2_9', 'State-trait scale 2_10',
        'State-trait scale 2_11', 'State-trait scale 2_12', 'State-trait scale 2_13', 'State-trait scale 2_14', 'State-trait scale 2_15',
        'State-trait scale 2_16', 'State-trait scale 2_17', 'State-trait scale 2_18', 'State-trait scale 2_19', 'State-trait scale 2_20',
        'State-trait scale 2_21', 'State-trait scale 2_22', 'State-trait scale 2_23', 'State-trait scale 2_24', 'State-trait scale 2_25',
        'State-trait scale 2_26', 'State-trait scale 2_27', 'State-trait scale 2_28', 'State-trait scale 2_29', 'State-trait scale 2_30',
        'State-trait scale 2_41', 'State-trait scale 2_32', 'State-trait scale 2_33', 'State-trait scale 2_34','State-trait scale 2_35',
-       'State-trait scale 2_36', 'State-trait scale 2_37', 'State-trait scale 2_38', 'State-trait scale 2_39',
-       'State-trait scale 2_40']]
+       'State-trait scale 2_36', 'State-trait scale 2_37', 'State-trait scale 2_38', 'State-trait scale 2_39','State-trait scale 2_40']]
 
-State_trait_scale_2_data.columns = ['unique_ID']+[i for i in xrange(1,41)]
+State_trait_scale_data.columns = ['unique_ID']+[i for i in xrange(1,81)]
 
-for col in list(State_trait_scale_2_data):
-       State_trait_scale_2_data[col] = State_trait_scale_2_data[col].astype(float)
+for col in list(State_trait_scale_data):
+       State_trait_scale_data[col] = State_trait_scale_data[col].astype(float)
 
-State_trait_scale_2_data.set_index('unique_ID',inplace=True)
+State_trait_scale_data.set_index('unique_ID',inplace=True)
 
 # scoring:
-# State_trait_scale_2_score=State_trait_scale_2_scoring(State_trait_scale_2_data)
-
-
+State_trait_scale_score=State_trait_scale_scoring(State_trait_scale_data)
+print State_trait_scale_score
 
 ##scs data
 scs_data=pt_2_data[['unique ID','SCS_1', 'SCS_2', 'SCS_3', 'SCS_4', 'SCS_5', 'SCS_6', 'SCS_7', 'SCS_8', 'SCS_9', 'SCS_10', 'SCS_11',
@@ -123,7 +107,6 @@ scs_data.set_index('unique_ID',inplace=True)
 # scoring:
 scs_score=SCS_scoring(scs_data)
 
-print scs_score
 
 
 ##Five_Dimensional_Curiosity data
@@ -156,5 +139,5 @@ CEI_2_data.set_index('unique_ID',inplace=True)
 CEI_2_score=CEI_2_scoring(CEI_2_data)
 
 #all pt1 concat
-# all_pt2_matan = pd.concat([Godspeed_score ,State_trait_scale_1_score,State_trait_scale_2_score,scs_score,_5_dc_score,CEI_2_score], axis=1)
+# all_pt2_matan = pd.concat([Godspeed_score ,State_trait_scale_score,scs_score, Five_Dimensional_Curiosity_score ,CEI_2_score], axis=1)
 # all_pt2_matan.to_csv('data/qualtrics/pt.2/processed_pt2.csv')
