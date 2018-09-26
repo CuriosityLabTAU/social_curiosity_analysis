4dbfd53903e53dab813bb0e1dc3f081ad880b4ba
“cltl1t405” <gorenlaptop1t405@gmail.com>
2018-09-26 19:47:43 +0300

    t test

--- plots_for_HRI.py
+++ plots_for_HRI.py
@@ -6,16 +6,17 @@ import seaborn as sns
 import matplotlib.gridspec as gridspec
 sns.set()
 import statsmodels.formula.api as sm
+from scipy import stats
 
-# #Matan
-# external_filename = 'data/all_external_data.csv'
-# internal_filename = 'data/external_and_internal_data/all_internal_data.csv'
+# #Matan lab
+external_filename = 'data/all_external_data.csv'
+internal_filename = 'data/external_and_internal_data/all_internal_data.csv'
 
 
 #Goren
-data_path = 'C:/Goren/CuriosityLab/Data/social_curiosity/'
-external_filename = data_path+'all_data/all_external_data.csv'
-internal_filename = data_path+'all_data/all_internal_data.csv'
+# data_path = 'C:/Goren/CuriosityLab/Data/social_curiosity/'
+# external_filename = data_path+'all_data/all_external_data.csv'
+# internal_filename = data_path+'all_data/all_internal_data.csv'
 
 
 all_internal = pd.read_csv(open(internal_filename))
@@ -23,11 +24,12 @@ all_internal.set_index('Subject_ID', inplace=True)
 all_internal.dropna(inplace=True)
 
 def AMT_histogram():
-    AMT_data = pd.read_csv('data/AMT_row.csv')
+    AMT_data = pd.read_csv('data/amt_probs/probs_from_AMT.csv')
     # preprocessing:
     columns = ['Duration (in seconds)', 'Finished', 'RecordedDate', 'Q1.2', 'Q1.3', 'Q1.4', 'Q1.5', 'Q0_11', 'Q1_11',
                'Q2_11', 'Q3_11', 'Q4_11', 'Q5_11',
                'Q6_11', 'Q7_11', 'Q8_11', 'Q9_11', 'Q10_11', 'Q11_11', 'Q12_11', 'Q13_11', 'Q14_11', 'validation_11']
+
     columns_names = ['duration', 'finished', 'date', 'agree_to_participate', 'gender', 'age', 'degree', '0', '1', '2',
                      '3', '4', '5',
                      '6', '7', '8', '9', '10', '11', '12', '13', '14', 'validation']
@@ -78,7 +80,6 @@ def AMT_histogram():
         a.set(xlabel='Relationship Representation ', ylabel='Frequency',title='Guster Number = '+str(i),ylim=(0, 40),xlim=(0, 1))
         a.axes.set_title('Guster Number = '+str(i), weight='bold')
     plt.show()
-
 # AMT_histogram()
 
 def b_over_time():
@@ -125,9 +126,7 @@ def b_over_time():
     print 'b_sequence vs time '
     result = sm.ols(formula='Time ~ Score ', data=b_sequence_data).fit()
     print result.summary()
-
-
-b_over_time()
+# b_over_time()
 
 def delta_over_time():
     f, ax = plt.subplots(1, 1)
@@ -149,6 +148,10 @@ def delta_over_time():
 
     ax.axes.set_title(r'$\delta$ and $\delta$~  over time')
 
+    random = (1.0 / 3 + (1.0 / 3) * (1.0 / 3) + (1.0 / 3) * (1.0 / 2)) * 2
+
+    ax.axhline(linewidth=2, color='r', y=random, linestyle=':')
+
     ax.legend()
 
     plt.show()
@@ -162,4 +165,44 @@ def delta_over_time():
     result = sm.ols(formula='Time ~ Score ', data=delta_tilde_data).fit()
     print result.summary()
 
-# delta_over_time()
+    delta_list=['delta_0','delta_1','delta_2','delta_3','delta_4']
+    delta_tilde_list = ['delta_tilde_0', 'delta_tilde_1','delta_tilde_2' ,'delta_tilde_3', 'delta_tilde_4']
+
+    tt_dict={}
+    for i in range(5):
+        B=all_internal[delta_list[i]].values.tolist()
+        C=all_internal[delta_tilde_list[i]].values.tolist()
+
+        ttb=stats.ttest_ind(B, [random]*len(B))
+        ttc=stats.ttest_ind(C, [random]*len(C))
+
+        tt_dict[i]={'delta -t':ttb[0],'delta -p':ttb[1]/2,'delta~ -t':ttc[0],'delta~ -p':ttc[1]/2}
+
+    tt_df = pd.DataFrame.from_dict(tt_dict, orient='index')
+    print tt_df
+
+
+delta_over_time()
+
+def delta_vs_b():
+    b_local_list=['b_local_0','b_local_1','b_local_2','b_local_3','b_local_4']
+    delta_tilde_list = ['delta_tilde_0', 'delta_tilde_1','delta_tilde_2' ,'delta_tilde_3', 'delta_tilde_4']
+
+    f, axes = plt.subplots(3, 2, constrained_layout=True)
+
+    for i in range(5):
+        data= all_internal[[b_local_list[i],delta_tilde_list[i]]]
+
+        a = sns.regplot(x=b_local_list[i], y=delta_tilde_list[i], data=data, x_estimator=np.mean ,ax=axes[int(i / 2), i % 2],)
+        title=r"$\delta$~_"+str(i)+" vs "+r"$\beta_{\rm local}$_"+str(i)
+        a.set(xlabel='Relationship Representation ', ylabel='Frequency',)
+        a.axes.set_title(title)
+
+        print title
+        result = sm.ols(formula=b_local_list[i]+'~'+delta_tilde_list[i], data=data).fit()
+        print result.summary()
+
+    plt.show()
+# delta_vs_b()
+
+
